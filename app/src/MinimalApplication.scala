817a94a26ec7786d98019860e830a36987f2ca57
lai <doublewlai@gmail.com>
2022-04-23 22:41:04 +0300

    make input interactive

--- app/src/MinimalApplication.scala
+++ app/src/MinimalApplication.scala
@@ -2,6 +2,7 @@ package app
 import scalatags.Text.all._
 object MinimalApplication extends cask.MainRoutes{
 
+  var messages = Vector(("alice", "Hello World!"), ("bob", "I am cow, hear me moo"))
   val bootstrap = "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.css"
   @cask.get("/")
   def hello() = doctype("html")(
@@ -9,8 +10,15 @@ object MinimalApplication extends cask.MainRoutes{
       head(link(rel := "stylesheet", href := bootstrap)),
       body(
         div(cls := "container")(
-          h1("Hello!"),
-          p("World!")
+          h1("Scala Chat!"),
+          div(
+            for ((name, msg) <- messages) yield p(b(name), " ", msg)
+          ),
+          form(action :="/", method := "post")(
+            input(`type` := "text", name := "name", placeholder := "User name"),
+            input(`type` := "text", name := "msg", placeholder := "Write a message!"),
+            input(`type` := "submit")
+          )
         )
       )
     )
