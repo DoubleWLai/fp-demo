96847869d7bee472b61099d8a0cb297313efa673
lai <doublewlai@gmail.com>
2022-04-24 10:28:13 +0300

    keep name in place

--- app/src/MinimalApplication.scala
+++ app/src/MinimalApplication.scala
@@ -5,7 +5,7 @@ object MinimalApplication extends cask.MainRoutes{
   var messages = Vector(("alice", "Hello World!"), ("bob", "I am cow, hear me moo"))
   val bootstrap = "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.css"
   @cask.get("/")
-  def hello(errorOpt: Option[String] = None) = doctype("html")(
+  def hello(errorOpt: Option[String] = None, userName: Option[String] = None, msg: Option[String] = None) = doctype("html")(
     html(
       head(link(rel := "stylesheet", href := bootstrap)),
       body(
@@ -16,8 +16,8 @@ object MinimalApplication extends cask.MainRoutes{
           ),
           for (error <- errorOpt) yield i(color.red)(error),
           form(action :="/", method := "post")(
-            input(`type` := "text", name := "name", placeholder := "User name"),
-            input(`type` := "text", name := "msg", placeholder := "Write a message!"),
+            input(`type` := "text", name := "name", placeholder := "User name", userName.map(value := _)),
+            input(`type` := "text", name := "msg", placeholder := "Write a message!", msg.map(value := _)),
             input(`type` := "submit")
           )
         )
@@ -27,11 +27,11 @@ object MinimalApplication extends cask.MainRoutes{
 
   @cask.postForm("/")
   def postChatMsg(name: String, msg: String) = {
-    if (name == "") hello(Some("Name cannot be empty"))
-    else if (msg == "") hello(Some("Message cannot be empty"))
+    if (name == "") hello(Some("Name cannot be empty"), Some(name), Some(msg))
+    else if (msg == "") hello(Some("Message cannot be empty"), Some(name), Some(msg))
     else {
       messages = messages :+ (name -> msg)
-      hello()
+      hello(None, Some(name), None)
     }
   }
 
