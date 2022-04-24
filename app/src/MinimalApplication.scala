5b7ee96873e2d45d35d4f02297535d02c8317929
lai <doublewlai@gmail.com>
2022-04-24 09:41:44 +0300

    update the post api

--- app/src/MinimalApplication.scala
+++ app/src/MinimalApplication.scala
@@ -24,9 +24,10 @@ object MinimalApplication extends cask.MainRoutes{
     )
   )
 
-  @cask.post("/do-thing")
-  def doThing(request: cask.Request) = {
-    request.text().reverse
+  @cask.postForm("/")
+  def postChatMsg(name: String, msg: String) = {
+    messages = messages :+ (name -> msg)
+    hello()
   }
 
   initialize()
