a50b5bb0b1fc11331befdc7c7742756e21e46a33
lai <doublewlai@gmail.com>
2022-04-25 22:59:29 +0300

    add websocket

--- app/src/MinimalApplication.scala
+++ app/src/MinimalApplication.scala
@@ -41,5 +41,14 @@ object MinimalApplication extends cask.MainRoutes{
     }
   }
 
+  var openConnections = Set.empty[cask.WsChannelActor]
+
+  @cask.websocket("/subscribe")
+  def subscribe() = cask.WsHandler { connection =>
+      connection.send(cask.Ws.Text(messageList().render))
+      openConnections += connection
+      cask.WsActor { case cask.Ws.Close(_, _) => openConnections -= connection }
+  }
+
   initialize()
 }
