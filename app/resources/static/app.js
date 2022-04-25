620c52851f97402ceaa46aaac5e1945e8a3463d9
lai <doublewlai@gmail.com>
2022-04-25 22:29:31 +0300

    render partial page

---
+++ app/resources/static/app.js
@@ -0,0 +1,14 @@
+function submitForm() {
+app/resources/static/app.js
+fetch(
+"/",
+{method: "POST", body: JSON.stringify({name: nameInput.value, msg: msgInput.value})}
+).then(response => response.json())
+.then(json => {
+if (json["success"]) {
+messageList.innerHTML = json["txt"]
+msgInput.value = ""
+}
+errorDiv.innerText = json["err"]
+})
+}
