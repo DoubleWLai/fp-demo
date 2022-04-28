82a4c1bb4ef123e6e2bb2e4625a73cae7c34d958
lai <doublewlai@gmail.com>
2022-04-28 20:33:33 +0300

    access to database

--- app/src/MinimalApplication.scala
+++ app/src/MinimalApplication.scala
@@ -2,7 +2,27 @@ package app
 import scalatags.Text.all._
 object MinimalApplication extends cask.MainRoutes{
 
-  var messages = Vector(("alice", "Hello World!"), ("bob", "I am cow, hear me moo"))
+  case class Message(name: String, msg: String)
+  import com.opentable.db.postgres.embedded.EmbeddedPostgres
+  val server = EmbeddedPostgres.builder()
+    .setDataDirectory(System.getProperty("user.home") + "/data")
+    .setCleanDataDirectory(false).setPort(5432)
+    .start()
+  import io.getquill._
+  import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
+  val pgDataSource = new org.postgresql.ds.PGSimpleDataSource()
+  pgDataSource.setUser("postgres")
+  val hikariConfig = new HikariConfig()
+  hikariConfig.setDataSource(pgDataSource)
+  val ctx = new PostgresJdbcContext(LowerCase, new HikariDataSource(hikariConfig))
+  ctx.executeAction("CREATE TABLE IF NOT EXISTS message (name text, msg text);")
+  import ctx._
+
+  def messages = ctx.run(query[Message].map(m => (m.name, m.msg)))
+
+
+
+
   val bootstrap = "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.css"
 
   @cask.staticResources("/static")
